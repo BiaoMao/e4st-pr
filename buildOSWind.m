7d31a4e80430d3e0db11ef362ba974001d3ae53c
Biao Mao <biaomao@gmail.com>
2017-01-23 19:36:22 -0500

    Add oswind to mpc.

---
+++ buildOSWind.m
@@ -0,0 +1,96 @@
+function [mpc, offer] = buildOSWind(mpc, offer, caseInfo, location, oswSize, verbose)
+%% buildOSWind: add buildable offshore wind
+%
+%   E4ST
+%   Copyright (c) 2012-2016 by Power System Engineering Research Center (PSERC)
+%   by Biao Mao, Rensselaer Polytechnic Institute
+%
+%   This file is part of E4ST.
+%   Covered by the 3-clause BSD License (see LICENSE file for details).
+%   See http://e4st.com/ for more info.
+    
+    % Set default argin
+    if nargin < 6
+        verbose = 1; % show a little debug information
+    end
+
+    % Initial data
+    windInfo = caseInfo.osWind;
+    idxNew = strcmp(windInfo{:, 'location'}, location) &...
+            strcmp(windInfo{:, 'size'}, oswSize);
+    newCap = windInfo{idxNew, 'cap'}(1);    
+    preIdx = size(mpc.gen, 1);
+
+    % Build wind
+    fuelType = 'oswind';
+    busId = windInfo{idxNew, 'bus'}; % get unique bus id
+    numBus = length(busId);
+
+    % Initial all the new table
+    newGen = zeros(numBus,21);
+    newGencost = zeros(numBus, size(mpc.gencost, 2));
+    newGenfuel = cell(numBus,1);
+    newGenaux = zeros(numBus,10);
+    newGenindex = zeros(numBus,1);
+    newOffer = zeros(numBus,13);
+
+    % Add new gen table             
+    newGen(:, 1) = busId; % bus number
+    newGen(:,6) = 1; % voltage magnitude setpoint
+    newGen(:,7) = 100; % MVA base
+    newGen(:,8) = 1; % gen status
+    newGen(:,9) = newCap; % PMAX
+    newGen(:,18) = Inf; % ramp rate for 10 min 
+
+    % Add new gencost table
+    newGencost(:, 1:4) = repmat([2 0 0 2], numBus, 1);
+    newGencost(:, 5) = windInfo{idxNew, 'cost2keep'}; % gencost
+
+    % Add new gen fuel table
+    newGenfuel(:, 1) = {fuelType};
+
+    % Add new gen aux data table
+    % All zeros for wind
+
+    % Add new genbuilt and newgen
+    newGenindex = ones(numBus, 1);
+
+    % Add new AFs
+    startCol = find(strcmp(windInfo.Properties.VariableNames, 'C1')); % Starting of AF table
+    newAf = windInfo{idxNew, startCol : end};
+
+    % Add new offer table
+    newOffer(:, 1) = windInfo{idxNew, 'cost2build'}; % fixed cost
+    newOffer(:, 2) = newCap; % Installed Cap
+    newOffer(:, 3) = 0;
+    newOffer(:, 4) = Inf;
+
+    % Update in the mpc and offer            
+    mpc.gen = [mpc.gen; newGen];
+    mpc.gencost = [mpc.gencost; newGencost];
+    mpc.genfuel = [mpc.genfuel; newGenfuel];
+    mpc.gen_aux_data = [mpc.gen_aux_data; newGenaux];          
+    mpc.newgen = [mpc.newgen; newGenindex];
+    mpc.availability_factor = [mpc.availability_factor; newAf];
+    mpc.ng = size(mpc.gen,1);
+    mpc.nb = size(mpc.bus,1);
+    offer = [offer; newOffer];
+
+    % Add capacity constraints
+    if ~isfield(mpc, 'caplim') || isempty(mpc.caplim)
+        mpc.caplim.map = [];
+        mpc.caplim.max = [];
+        mpc.caplim.min = [];
+    end
+    idxOsw = preIdx + 1 : preIdx + numBus;
+    idxGen = zeros(1, size(mpc.gen, 1));
+    idxGen(idxOsw) = 1;
+    mpc.caplim.map = [mpc.caplim.map; idxGen];
+    mpc.caplim.max = [mpc.caplim.max; newCap];
+    mpc.caplim.min = [mpc.caplim.min; newCap];
+
+    % Debug information
+    if verbose == 1
+        fprintf('Buildable %s %s are added at %s\n', oswSize, fuelType, location);
+    end
+end
--- updateConsDim.m
+++ updateConsDim.m
@@ -1,5 +1,6 @@
 function [mpc] = updateConsDim(mpc)
-%% updateConsDim: updated constraints dimentions after adding generators
+%% updateConsDim: updated constraints dims after adding generators
+%   Dims include total_output and caplim
 %
 %   E4ST
 %   Copyright (c) 2012-2016 by Power System Engineering Research Center (PSERC)
@@ -9,10 +10,14 @@ function [mpc] = updateConsDim(mpc)
 %   Covered by the 3-clause BSD License (see LICENSE file for details).
 %   See http://e4st.com/ for more info.
 
-    [n_contraints, old_ng] = size(mpc.total_output.map);
-    delta_ng = mpc.ng - old_ng;
-    mpc.total_output.map = [mpc.total_output.map zeros(n_contraints, delta_ng)];        
-    
-    mpc.total_output.coeff = [mpc.total_output.coeff; ones(delta_ng, size(mpc.total_output.coeff, 2))];
+    if isfield(mpc, 'total_output')
+        [n_contraints, old_ng] = size(mpc.total_output.map);
+        delta_ng = mpc.ng - old_ng;
 
+        if delta_ng > 0
+            mpc.total_output.map = [mpc.total_output.map zeros(n_contraints, delta_ng)];
+            mpc.total_output.coeff = [mpc.total_output.coeff;...
+                    ones(delta_ng, size(mpc.total_output.coeff, 2))];
+        end
+    end
  
